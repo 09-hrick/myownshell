b18a14ad79d03392e315fd274cbbde45c4d7bcb5
Sanyam Mahajan <smahajan@oneshield.com>
2024-08-29 00:56:48 +0530

    pass stage

--- src/main.cpp
+++ src/main.cpp
@@ -1,15 +1,19 @@
 #include <iostream>
 
-int main() {
+int main()
+{
   // Flush after every std::cout / std:cerr
-  while(true)
- { std::cout << std::unitbuf;
-  std::cerr << std::unitbuf;
-
-  // Uncomment this block to pass the first stage
-  std::cout << "$ ";
-  
   std::string input;
-  std::getline(std::cin, input);
-  std::cout << input << ": command not found\n";}
+  while (input != "exit 0")
+  {
+    std::cout << std::unitbuf;
+    std::cerr << std::unitbuf;
+
+    // Uncomment this block to pass the first stage
+    std::cout << "$ ";
+
+    std::getline(std::cin, input);
+    std::cout << input << ": command not found\n";
+  }
+  return 0;
 }
