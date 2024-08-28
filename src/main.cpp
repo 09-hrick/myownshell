0722d75e93ac638da33b532d2b0bffdea7bbc2a0
Sanyam Mahajan <smahajan@oneshield.com>
2024-08-29 00:58:14 +0530

    pass stage

--- src/main.cpp
+++ src/main.cpp
@@ -4,10 +4,10 @@ int main()
 {
   // Flush after every std::cout / std:cerr
   std::string input;
+  std::cout << std::unitbuf;
+  std::cerr << std::unitbuf;
   while (input != "exit 0")
   {
-    std::cout << std::unitbuf;
-    std::cerr << std::unitbuf;
 
     // Uncomment this block to pass the first stage
     std::cout << "$ ";
