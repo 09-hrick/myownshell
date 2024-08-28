26fa6915be99c4657cf29221b6f4b9926e6e38bb
Sanyam Mahajan <smahajan@oneshield.com>
2024-08-29 01:30:21 +0530

    'test'

--- src/main.cpp
+++ src/main.cpp
@@ -6,12 +6,30 @@ int main()
   std::string input;
   std::cout << std::unitbuf;
   std::cerr << std::unitbuf;
+  std::string builtin[3] = {"echo", "exit", "type"};
   std::cout << "$ ";
   while (std::getline(std::cin, input) && input != "exit 0")
   {
     if (input.find("echo") == 0)
     {
       std::cout << input.substr(5, input.size()) << std::endl;
+    }
+    else if (input.find("type") == 0)
+    {
+      std::string cmd = input.substr(5, input.size());
+      bool chkvalid = 0;
+      for (std::string &builtins : builtin)
+      {
+        if (cmd.substr(0, 5) == builtins)
+        {
+          std::cout << cmd << " is a shell builtin\n";
+          chkvalid = 1;
+        }
+      }
+      if (!chkvalid)
+      {
+        std::cout << input << std::endl;
+      }
     }
     else
     {
