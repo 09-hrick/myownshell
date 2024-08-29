3fe2946ab996fbeb0cb637d8898495cad886e124
Sanyam Mahajan <smahajan@oneshield.com>
2024-08-29 12:41:55 +0530

    pass stage

--- src/main.cpp
+++ src/main.cpp
@@ -2,95 +2,144 @@
 #include <string>
 #include <sstream>
 #include <filesystem>
+#include <stdio.h>
+
 using namespace std;
+
 enum validCommands
 {
-  echo,
-  cd,
-  exit0,
-  type,
-  cat,
-  invalid,
+    echo,
+    cd,
+    exit0,
+    pwd,
+    type,
+    invalid,
 };
-validCommands isValid(std::string command)
-{
-  command = command.substr(0, command.find(" "));
-  if (command == "echo")
-    return validCommands::echo;
-  if (command == "cd")
-    return validCommands::cd;
-  if (command == "cat")
-    return validCommands::cat;
-  if (command == "exit")
-    return validCommands::exit0;
-  if (command == "type")
-    return validCommands::type;
-  return invalid;
+
+validCommands isValid(std::string command){
+    command = command.substr(0,command.find(" "));
+
+    if(command == "echo") return validCommands::echo;
+    if(command == "cd") return validCommands::cd;
+    if(command == "exit") return validCommands::exit0;
+    if(command == "type") return validCommands::type;
+    if(command == "pwd") return validCommands::pwd;
+
+    return invalid;
 }
-std::string valid[4] = {"echo", "cd", "exit0"};
-std::string get_path(std::string command)
-{
-  std::string path_env = std::getenv("PATH");
-  std::stringstream ss(path_env);
-  std::string path;
-  while (!ss.eof())
-  {
-    getline(ss, path, ':');
-    string abs_path = path + '/' + command;
-    if (filesystem::exists(abs_path))
-    {
-      return abs_path;
+
+std::string valid[5] = {"echo", "cd", "exit0", "pwd", "type"};
+
+std::string cwd = filesystem::current_path().string();
+
+std::string HOME = getenv("HOME");
+
+std::string get_path(std::string command){
+    std::string path_env = std::getenv("PATH");
+
+    std::stringstream ss(path_env);
+    std::string path;
+
+    while(!ss.eof()){
+        getline(ss, path, ':');
+
+        string abs_path = path + '/' + command;
+
+        if(filesystem::exists(abs_path)){
+            return abs_path;
+        }
     }
-  }
-  return "";
+    return "";  
 }
-int main()
-{
-  // You can use print statements as follows for debugging, they'll be visible when running tests.
-  // std::cout << "Logs from your program will appear here!\n";
-  bool exit = false;
-  while (!exit)
-  {
-    // Flush after every std::cout / std:cerr
-    std::cout << std::unitbuf;
-    std::cerr << std::unitbuf;
-    std::cout << "$ ";
-    std::string input;
-    std::getline(std::cin, input);
-    switch (isValid(input))
-    {
-    case cd:
-      break;
-    case echo:
-      input.erase(0, input.find(" ") + 1);
-      std::cout << input << "\n";
-      break;
-    case exit0:
-      exit = true;
-      break;
-    case type:
-      input.erase(0, input.find(" ") + 1);
-      if (isValid(input) != invalid)
-      {
-        std::cout << input << " is a shell builtin\n";
-      }
-      else
-      {
-        std::cout << input << " not found\n";
-        std::string path = get_path(input);
-        if (path.empty())
-        {
-          std::cout << input << " not found\n";
+
+int main() {
+
+    // You can use print statements as follows for debugging, they'll be visible when running tests.
+    // std::cout << "Logs from your program will appear here!\n";
+
+    bool exit = false;
+
+    while (!exit){
+        // Flush after every std::cout / std:cerr
+        std::cout << std::unitbuf;
+        std::cerr << std::unitbuf;
+        std::cout << "$ ";
+        std::string input;
+        std::getline(std::cin, input);
+
+        switch(isValid(input)){
+            case cd:
+                {
+                string dir = input.substr(input.find(" ")+1); 
+
+                if(dir=="~"){
+                    dir = HOME;
+                }
+                else if(!dir.starts_with('/')){
+                    dir = cwd + '/' + dir;
+                }
+
+                if(filesystem::is_directory(dir)){
+                    // Change directory
+                    cwd = filesystem::canonical(dir);
+                }   
+                else{
+                    std::cout<<dir<<": No such file or directory\n";
+                }
+                break;
+                }
+            case echo:
+                input.erase(0,input.find(" ")+1);
+                std::cout<<input<<"\n";
+                break;
+            case exit0:
+                exit=true;
+                break;
+            case pwd:
+                std::cout<<cwd<<endl;
+                break;
+            case type:
+                input.erase(0,input.find(" ")+1);
+                if(isValid(input) != invalid){
+                    std::cout<<input<<" is a shell builtin\n";
+                }
+                else{
+                    std::string path = get_path(input);
+
+                    if(path.empty()){
+                        std::cout<<input<<": not found\n";
+                    }
+                    else{
+                        std::cout<<input<<" is "<<path<<std::endl;
+                    }
+                }
+                break;
+            default:
+                string command = input.substr(0,input.find(" "));
+
+                string path = get_path(command);
+
+                if(path.empty()){
+                    std::cout<<input<<": command not found\n";
+                }
+                else{
+                    string full = path + input.substr(command.length());
+
+                    char buffer[128];
+                    string result;
+
+                    FILE *pipe = popen(full.c_str(), "r");
+
+                    while(!feof(pipe)){
+                        if(fgets(buffer, 128, pipe) != NULL){
+                            cout<<buffer;
+                        }
+                    }
+
+                    pclose(pipe);
+                }
+                break;
         }
-        else
-        {
-          std::cout << input << " is " << path << std::endl;
-        }
-      }
-      break;
-    default:
-      std::cout << input << ": command not found\n";
-      break;
     }
-  }
+
 }
